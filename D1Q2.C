9a3480b8ca77ba7514e23f7de259fc10a6318405
Trishal-Chadha <trishalchadha@gmail.com>
2025-08-31 17:32:23 +0530

    Initial commit

---
+++ D1Q2.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("ENTER ANY TWO NUMBERS");
+    int a, b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("sum=%d\n",a+b);
+printf("subtraction=%d\n",a-b);
+printf("multiplication=%d\n",a*b);
+printf("division=%d\n",a/b);
+
+    return 0;
+}
