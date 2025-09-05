9869957ba2fcbef4d0e41eb23c49c38bd8877afc
Trishal-Chadha <trishalchadha@gmail.com>
2025-09-05 18:24:09 +0530

    Initial commit

---
+++ DAY16Q1.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    int binary[32]; 
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    i = 0;
+    while (num > 0) {
+        binary[i] = num % 2; 
+        num = num / 2;       
+        i++;
+    }
+
+    printf("Binary representation: ");
+    for (i = i - 1; i >= 0; i--) {
+        printf("%d", binary[i]);
+    }
+
+    return 0;
+}
