c1fe9c05c6df976f969c39d17d4ff0c9ad9f16eb
Trishal-Chadha <trishalchadha@gmail.com>
2025-09-15 00:20:36 +0530

    Initial commit

--- DAY25Q2.C
+++ DAY25Q2.C
@@ -1,21 +1,13 @@
 #include <stdio.h>
+
 int main() {
     int i, j;
 
-    for (i = 1; i <= 5; i += 2) {
-        for (j = 1; j <= i; j++) {
-            printf("*\n");
+    for (i = 5; i >= 1; i--) {       
+        for (j = 1; j <= i; j++) {   
+            printf("*");
         }
         printf("\n");  
-    }
-
-   
-    for (i = 3; i >= 1; i -= 2) {
-        for (j = 1; j <= i; j++) {
-            printf("*\n");
-        }
-        if (i != 1)  
-            printf("\n");
     }
 
     return 0;
