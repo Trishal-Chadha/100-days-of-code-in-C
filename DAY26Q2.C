7ff468d71a66dd87565df7561395604e9e30ef87
Trishal-Chadha <trishalchadha@gmail.com>
2025-09-18 16:32:25 +0530

    Initial commit

--- DAY26Q2.C
+++ DAY26Q2.C
@@ -1,5 +1,4 @@
 #include <stdio.h>
-sss
 int main() {
     int i, j;
     int rows[] = {1, 3, 5, 3, 1}; 
--- DAY27Q2.C
+++ DAY27Q2.C
@@ -4,11 +4,11 @@ int main() {
     int i, j, space, rows = 4;
 
     for (i = 1; i <= rows; i++) {
-        // Print spaces
+        
         for (space = 1; space <= rows - i; space++) {
             printf(" ");
         }
-        // Print stars
+    
         for (j = 1; j <= 2 * i - 1; j++) {
             printf("*");
         }
