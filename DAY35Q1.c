98fabcdbe485bb998a186eadee6f6cd638263e1d
Trishal-Chadha <trishalchadha@gmail.com>
2025-09-24 00:20:59 +0530

    Add files via upload

---
+++ DAY35Q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int first = -2147483648, second = -2147483648; // Minimum int values
+    for (i = 0; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    if (second == -2147483648) {
+        printf("No second largest element (all elements may be same).\n");
+    } else {
+        printf("Second largest element = %d\n", second);
+    }
+
+    return 0;
+}
