f3c6ff4123bb41fcb2d8029d8aa947ea999875f3
Trishal-Chadha <trishalchadha@gmail.com>
2025-09-30 23:30:15 +0530

    Add files via upload

---
+++ DAY41Q1.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);   
+
+
+    while(str[count] != '\0') {
+        count++;
+    }
+
+    printf("Total characters = %d\n", count);
+    return 0;
+}
