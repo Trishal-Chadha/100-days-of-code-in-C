370a0a1bc48283d180c9c56182475cffa3df1f2c
Trishal-Chadha <trishalchadha@gmail.com>
2025-10-02 23:59:24 +0530

    Add files via upload

---
+++ ASSIGNMENT_01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    char source[] = "Hello World";
+    char substring[10];
+    int start = 6; 
+    int length = 5; 
+    int i, j = 0;
+
+    for (i = start; i < start + length; i++) {
+        substring[j] = source[i];
+        j++;
+    }
+    substring[j] = '\0'; 
+
+    printf("%s", substring);
+    return 0;
+}
