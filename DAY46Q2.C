f9b6414fdab6c4dde9b0d8f64e381a2917ed066f
Trishal-Chadha <trishalchadha@gmail.com>
2025-10-05 00:13:21 +0530

    Add files via upload

---
+++ DAY46Q2.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int freq[26] = {0}, i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            if (freq[str[i] - 'a'] == 1) {
+                printf("First repeating lowercase alphabet: %c\n", str[i]);
+                return 0;
+            }
+            freq[str[i] - 'a']++;
+        }
+    }
+
+    printf("No repeating lowercase alphabet found.\n");
+    return 0;
+}
